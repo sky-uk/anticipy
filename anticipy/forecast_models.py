906b7692ca0731b5e281533db04cf362fe12b76e
capelastegui <pcapelastegui@gmail.com>
2018-10-02 13:23:10 +0100

    Fix docs bug: missing blank line

--- anticipy/forecast_models.py
+++ anticipy/forecast_models.py
@@ -1050,6 +1050,7 @@ def get_model_outliers(df, window=3):
     :type window:
     :return:
     :rtype:
+
     Note: due to the way the thresholds are defined, we require 6+ samples in series to find a spike.
     """
     is_mult = False
