ad591f4aa0767fb160fe1153ce375cfc2ae5ef5c
capelastegui <pcapelastegui@gmail.com>
2020-05-07 15:21:15 +0100

    bug #179; Italy calendar model error

--- anticipy/forecast_models.py
+++ anticipy/forecast_models.py
@@ -1681,7 +1681,7 @@ model_calendar_us = get_model_from_calendars(USFederalHolidayCalendar(),
                                              'calendar_us')
 # Calendar for Italy
 model_calendar_ita = get_model_from_calendars(
-    [CalendarChristmasUK(), CalendarBankHolUK()], 'calendar_uk')
+    [CalendarChristmasIta(), CalendarBankHolIta()], 'calendar_uk')
 
 
 def get_model_from_datelist(name=None, *args):
